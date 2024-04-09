b56d1c1334a3a6ffce99a90c4254c8800f1a6194
Deinigu <diegolopezreduello@gmail.com>
2024-04-09 18:25:58 +0200

    ✨Hough Transform

--- edge_detection.py
+++ edge_detection.py
@@ -1,9 +1,12 @@
+import math
 import os
+from statistics import mean
 import cv2
 import numpy as np
 
 # Load the image
-img = cv2.imread('edge-detection/assets/chessboard.png')
+name = "prueba"
+img = cv2.imread("edge-detection/assets/" + name + ".png")
 
 # Transform the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -14,41 +17,44 @@ blur = cv2.GaussianBlur(gray, (5, 5), 0)
 # Apply Canny edge detection
 edges = cv2.Canny(blur, 50, 150)
 
-# Find contours in the image
-contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# Hough Transform
+lines = cv2.HoughLines(edges, 1, np.pi / 180, 125, 100, 10)
+lines = np.reshape(lines, (-1, 2))
 
-max_area = 0
-chessboard_cnt = None
+# Separate the lines into vertical and horizontal lines
+h_lines, v_lines = [], []
+for rho, theta in lines:
+    if theta < np.pi / 4 or theta > np.pi - np.pi / 4:
+        v_lines.append([rho, theta])
+    else:
+        h_lines.append([rho, theta])
 
-for cnt in contours:
-    # Calculate the perimeter of the contour
-    epsilon = 0.02 * cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, epsilon, True)
+# Find and cluster the intersecting
+points = []
+for r_h, t_h in h_lines:
+    for r_v, t_v in v_lines:
+        a = np.array([[np.cos(t_h), np.sin(t_h)], [np.cos(t_v), np.sin(t_v)]])
+        b = np.array([r_h, r_v])
+        inter_point = np.linalg.solve(a, b)
+        points.append(inter_point)
 
-    # If the contour has 4 corners, it is the chessboard
-    if len(approx) == 4:
-        area = cv2.contourArea(cnt)
-        if area > max_area:
-            max_area = area
-            chessboard_cnt = approx
+points_shape = list(np.shape(points))
+points = np.reshape(points, (points_shape[0], points_shape[1]))
 
-# Draw the contours of the chessboard
-if chessboard_cnt is not None:
-    # Draw the contours of the chessboard
-    cv2.drawContours(img, [chessboard_cnt], -1, (0, 255, 0), 3)
+# Draw the points
+for point in points:
+    x, y = point
+    cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)
 
-    # Draw circles on the corners of the chessboard
-    for point in chessboard_cnt:
-        cv2.circle(img, tuple(point[0]), 5, (0, 0, 255), -1)
+cv2.imshow("Chessboard", img)
 
-# Show the image
-cv2.imshow('Result', img)
+# Wait for key press to close the windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Create results directory if it does not exist
-if not os.path.exists('edge-detection/results'):
-    os.makedirs('edge-detection/results')
+if not os.path.exists("edge-detection/results"):
+    os.makedirs("edge-detection/results")
 
 # Save the image with the detected edges
-cv2.imwrite('edge-detection/results/chessboard_edges.png', img)
+cv2.imwrite("edge-detection/results/" + name + "_edges.png", img)
